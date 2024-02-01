4785e5dc178d7dba185dfb7f183a61ed7742728a
ReverendSafari <s.swanson24@ncf.edu>
2024-02-01 13:46:15 -0500

    Forgot to save

--- Connection.java
+++ Connection.java
@@ -2,6 +2,15 @@ import java.io.*;
 import java.net.Socket;
 import java.net.ServerSocket;
 
+
+
+/**
+ * The Connection class implements a simple TCP server that listens on a specified port
+ * and handles incoming client connections. Each client connection is managed in a separate thread
+ * to allow concurrent handling of multiple clients. The server echoes back any data received from the client.
+ * 
+ * The server runs indefinitely, listening for and accepting new connections, until externally terminated.
+ */
 public class Connection {
     
     public static void main(String[] args) throws IOException {
@@ -13,13 +22,19 @@ public class Connection {
 
         //For loop to wait and listen to socket for a request
         try {
+            //While Loop runs perpetually 
             while(true) {
                 System.out.println("WAITING FOR CONNECTION...");
+                //Generates the socket we will be sending data to
                 Socket clientSocket = serverSocket.accept();
+                //Prints the connected ip address
                 System.out.println("Connection ACCEPTED from..." + clientSocket.getInetAddress());
 
+                //Creates a new thread, and passes it a new "runnable" object with our clientsocket 
+                //Passing the client connection into a thread allows our program to handle multiple connections concurrently
                 new Thread(new HandleConnection(clientSocket)).start();
             }
+        //"Closes" the connection, however our current code doesn't reach this block -- Future update    
         } finally {
             serverSocket.close();
         }
@@ -27,26 +42,44 @@ public class Connection {
     }    
 }
 
+/**
+ * The HandleConnection class is responsible for handling individual client connections to the server.
+ * It implements the Runnable interface, allowing each connection to be processed in a separate thread.
+ * This class manages reading data from the client, echoing it back, and closing the connection when done.
+ */
 class HandleConnection implements Runnable {
+    //The socket we want to communicate with
     private Socket clientSocket; 
 
+    //Constructor
     public HandleConnection(Socket sock) {
         this.clientSocket = sock;
     }
+
+    //Derived from runnable (Allows us to assign a "Task" and have it run separetely in different threads)
     public void run() {
+        //Input stream concerts byte stream to character stream
         try (BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            //PrintWriter translates our return data for the OutputStream
             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true)) {
 
+            //Current Line taken from input stream
             String inputLine;
+            //While their is data left (Inputline isn't null)
             while ((inputLine = in.readLine()) != null) {
+                //Print to console 
                 System.out.println("RECEIVED: " + inputLine);
+                //Send to the output stream for the client to recieve
                 out.println(inputLine);
             }
+        //Catch IO error if there is a problem intializing the readers
         } catch (IOException e) {
             System.out.println("Exception in ConnectionHandler: " + e.getMessage());
+        //Try to close the client socket
         } finally {
             try {
                 clientSocket.close();
+            //Print the IO error if we can't close it
             } catch (IOException e) {
                 System.out.println("Error closing client socket: " + e.getMessage());
             }
--- README.md
+++ README.md
@@ -1,5 +1,43 @@
 My attempt to learn more about TCP/IP protocals and webservers
 I did some prelimanary reading on TCP/UDP and basic HTTP stuff
 
-CURRENT GOAL:
-Write a simple TCP web server that can recieve data, and echo it back to the client.
+# Simple TCP Echo Server
+
+This repository contains a simple TCP echo server implemented in Java. The server listens on a specified port for client connections and echoes back any data it receives. Each client connection is handled in a separate thread, allowing for concurrent processing of multiple clients.
+
+## Getting Started
+
+### Prerequisites
+
+- Java Development Kit (JDK) 11 or higher.
+
+### Running the Server
+
+1. **Clone the Repository**
+
+If you haven't already, clone the repo :)
+
+'git clone <repo_url>'
+
+2. **Compile the server**
+
+We must compile to generate our class file to run
+'cd path/to/directory'
+'javac Connection.java'
+
+3. **Start the server**
+
+Now we can finally run our 'Connection.class' file to start the server 
+Make sure you are still in the project directory, and then run
+'java Connection'
+
+You know you succeeded if you see The server is listening to PORT 6789
+"WAITING FOR CONNECTION... in your terminal"
+
+4. **Connect to the server** 
+
+Open a separate terminal, we can use a TCP client like telnet to test the server
+'telnet localhost 6789'
+
+Upon success you should see something like "Connected to <IP HERE>"
+Now connected you can type any text into your terminal and click enter, and you should see your message echoed back! 
